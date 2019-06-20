2642f333c4cb7a70654d7ecdfd7b1b815a874dfc
Kyle Olson <kaollie74@gmail.com>
2019-06-20 11:53:50 -0500

    we had the awesome pop up to work

--- js/client.js
+++ js/client.js
@@ -6,8 +6,7 @@ const playfair = [
     'Mitch', 'Jake'
 ]
 
-$(document).ready(onReady); // .ready() is an event which calls
-// the onReady function.                             function onReady(){
+$(document).ready(onReady); 
 
 // function calls renderStudentList () and passes
 // playfair array into it.  
@@ -15,7 +14,26 @@ function onReady() {
 
     console.log('jQuery works');
     renderStudentList(playfair);
+
+    // Need to select something alreay on the DOM
+    // can filter the event to something new (dynamically generated)
+    // do this with the 2nd arg to on ('.student )
+    $('#students').on('click', '.student', showAwesomeAlert) // the '.student' was created at
+                                                            // the bottom to specify on who we
+                                                            //actually clicked.
 }
+
+// This function will be called when we click on a student
+// it will show an alert saying tht student is awesome.
+function showAwesomeAlert(event){
+    console.log('Event', event);
+    console.log(this);// 'this' is contextual it varies depending on where it is.
+    
+    alert('Check console.log')
+    
+}
+
+
 // this function will take in an array of students,
 // loop the our students & display them on the DOM
 function renderStudentList(studentList) {
@@ -26,5 +44,5 @@ function renderStudentList(studentList) {
 // This will take in a single student,
 // and append it to the '#student' id on HTML page.
 function renderStudent(student) {
-    $('#students').append(`<li>${student}</li>`)
+    $('#students').append(`<li class="student">${student}</li>`)
 }
